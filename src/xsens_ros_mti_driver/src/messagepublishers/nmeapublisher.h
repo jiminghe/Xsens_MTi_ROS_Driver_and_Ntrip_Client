7d18e1809849609152bd332bbbe5fc4f8c1807b9
jiminghe <89671976+jiminghe@users.noreply.github.com>
2023-04-26 23:26:38 +0800

    Update nmeapublisher.h

--- src/xsens_ros_mti_driver/src/messagepublishers/nmeapublisher.h
+++ src/xsens_ros_mti_driver/src/messagepublishers/nmeapublisher.h
@@ -33,14 +33,6 @@
 #include <xscommon/ntrip_util.h>
 
 
-
-//mti-680 mt manager string report: $GPGGA,122704.02,3108.980821,N,12120.811829,E,5,31,0.8,19.71,M,29.33,M,0,158*50
-//ros driver /gnss: latitude: 31.1496818, longitude: 121.3468632, altitude: 29.266000000000002
-// ros driver /nmea, this one: sentence: "$GPGGA,122508.2,318.98090200,N,12120.81179200,E,3,29,0.8,19.700,M,,M,,*46\r\n"
-// ntrip_client.cpp, line 244: gga_buffer_ is $GPGGA,122848.00,3108.9799180,N,12120.8119420,E,1,10,1.2,0.0000,M,-2.860,M,,0000*40
-
-
-
 struct NMEAPublisher : public PacketCallback
 {
     ros::Publisher pub;
