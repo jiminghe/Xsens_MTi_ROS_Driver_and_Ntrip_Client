4ce1da80cfa40667ce9f78625326b0907d9d340a
jiminghe <89671976+jiminghe@users.noreply.github.com>
2023-08-21 08:36:00 +0800

    Update xsens_time_handler.h
    add mutex

--- src/xsens_ros_mti_driver/src/xsens_time_handler.h
+++ src/xsens_ros_mti_driver/src/xsens_time_handler.h
@@ -10,13 +10,16 @@ public:
     XsensTimeHandler();
     ros::Time convertUtcTimeToRosTime(const XsDataPacket &packet);
     void setTimeOption(const std::string &option);
+    void setRollover(const uint32_t &rollOver);
 
 private:
     std::string time_option;
     ros::Time firstUTCTimestamp;
     uint32_t prevSampleTimeFine;
     bool isFirstFrame;
-    static const uint32_t ROLLOVER = 0xFFFFFFFF;  // 2^32-1
+    uint32_t m_RollOver = 0xFFFFFFFF;  // 2^32-1
+    mutable std::mutex m_mutex; // Mutex to protect the shared state
+     
 };
 
 #endif // XSENS_TIME_HANDLER_H
